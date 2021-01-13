a6d6c2b02f9941196a91be4445fdd5af5c66fea9
JohnGuilding <jwguilding@gmail.com>
2021-01-13 21:55:22 +0000

    added app colors and styled ppost borders

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -21,29 +21,31 @@ const Post = (props) => {
     const { user } = useContext(AuthContext);
 
     return (
-        <article className={styles.post}>
-            <h2 className={styles.user}>{username}</h2>
-            <p className={styles.body}>{body}</p>
-            <Link to={`/posts/${id}`} className={styles.date}>
-                <p>{moment(createdAt).fromNow(true)}</p>
-            </Link>
-
-            <div className={styles.likeBtn}>
-                <LikeButton user={user} post={{ id, likes, likeCount }}/>
+        <article className={styles.postBorder}>
+            <div className={styles.post}>
+                <h2 className={styles.user}>{username}</h2>
+                <p className={styles.body}>{body}</p>
+                <Link to={`/posts/${id}`} className={styles.date}>
+                    <p>{moment(createdAt).fromNow(true)}</p>
+                </Link>
+    
+                <div className={styles.likeBtn}>
+                    <LikeButton user={user} post={{ id, likes, likeCount }}/>
+                </div>
+    
+                <Link to={`/posts/${id}`}>
+                    <div className={styles.commentBtn}>
+                        <button>comment</button>
+                        <div>{commentCount}</div>
+                    </div>
+                </Link>
+    
+                {user && user.username === username && (
+                    <div className={styles.deleteBtn}>
+                        <DeleteButton postId={id} />
+                    </div>
+                )}
             </div>
-
-            <Link to={`/posts/${id}`}>
-                <div className={styles.commentBtn}>
-                    <button>comment</button>
-                    <div>{commentCount}</div>
-                </div>
-            </Link>
-
-            {user && user.username === username && (
-                <div className={styles.deleteBtn}>
-                    <DeleteButton postId={id} />
-                </div>
-            )}
         </article>
     );
 };
